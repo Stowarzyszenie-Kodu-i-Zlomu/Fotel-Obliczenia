eab0d8bab76ba95d9e4da47711b31573a9dd82db
Błażej Gałkowski <s30573@pjwstk.edu.pl>
2024-12-10 13:09:35 +0100

    dodano komentarze

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import numpy as np
-
+#set positions of pistons (x,y values)
 p1 = [0,10,0]
 p2 = [-5,-5,0]
 p3 = [5,-5,0]
+#height of zero position
 h0 = 50
+#set pitch and roll values
 pitch = 45
 roll = 0
 
@@ -38,18 +40,22 @@ def set_height(height_zero, vector):
      return height_zero + vector[2]
 
 def rotation(p1, p2, p3, height_zero, pitch, roll,):
+    #convert points to vectors
     v1 = point_to_vector(p1)
     v2 = point_to_vector(p2)
     v3 = point_to_vector(p3)
+    #calculate rotation matrix
     R = rotation_matrix(pitch,roll)
+    #rotate each vector
     v1 = R @ v1
     v2 = R @ v2
     v3 = R @ v3
+    #get height of each piston
     h1 = set_height(height_zero,v1)
     h2 = set_height(height_zero,v2)
     h3 = set_height(height_zero,v3)
     height = [h1,h2,h3]
-    print(v1)
+
     return height
 
 height = rotation(p1, p2, p3, h0, pitch, roll)
